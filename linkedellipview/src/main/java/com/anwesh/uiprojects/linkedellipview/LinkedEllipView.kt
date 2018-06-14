af8c924f9c7982f33077fa3237031a5489a66cb1
Anwesh43 <anweshthecool0@gmail.com>
2018-06-14 10:46:26 +0530

    adding LEAnimator class

--- linkedellipview/src/main/java/com/anwesh/uiprojects/linkedellipview/LinkedEllipView.kt
+++ linkedellipview/src/main/java/com/anwesh/uiprojects/linkedellipview/LinkedEllipView.kt
@@ -45,4 +45,32 @@ class LinkedEllipView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class LEAnimator(var view : View, var animated : Boolean = false) {
+
+        fun animate(cb : () -> Unit) {
+            if (animated) {
+                cb()
+                try {
+                    Thread.sleep(50)
+                    view.invalidate()
+                } catch (ex : Exception) {
+
+                }
+            }
+        }
+
+        fun start() {
+            if (!animated) {
+                animated = true
+                view.postInvalidate()
+            }
+        }
+
+        fun stop() {
+            if (animated) {
+                animated = false
+            }
+        }
+    }
 }
