980b0bec49a297d49657f6a3939ac81355e784c2
Anwesh43 <anweshthecool0@gmail.com>
2018-06-14 10:59:24 +0530

    adding LENode class

--- linkedellipview/src/main/java/com/anwesh/uiprojects/linkedellipview/LinkedEllipView.kt
+++ linkedellipview/src/main/java/com/anwesh/uiprojects/linkedellipview/LinkedEllipView.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.MotionEvent
 import android.graphics.*
 
+val LE_NODES = 5
+
 class LinkedEllipView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
@@ -71,6 +73,58 @@ class LinkedEllipView(ctx : Context) : View(ctx) {
             if (animated) {
                 animated = false
             }
+        }
+    }
+
+    data class LENode (var i : Int, val state : LEState = LEState()) {
+
+        private var next : LENode? = null
+
+        private var prev : LENode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < LE_NODES - 1) {
+                next = LENode(i +1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            prev?.draw(canvas, paint)
+            val w : Float = canvas.width.toFloat()
+            val h : Float = canvas.height.toFloat()
+            val gap : Float = w / LE_NODES
+            paint.style = Paint.Style.STROKE
+            paint.strokeWidth = Math.min(w, h) / 60
+            paint.color = Color.parseColor("#9b59b6")
+            canvas.save()
+            canvas.translate(w/2 + gap/2, h/2)
+            canvas.drawArc(RectF(-gap/2, -gap/4, gap/2, gap/4), 180f - 180f * state.scale, 2 * 180f * state.scale, false,paint)
+            canvas.restore()
+        }
+
+        fun update(stopcb : (Float) -> Unit) {
+            state.update(stopcb)
+        }
+
+        fun startUpdating(startcb : () -> Unit) {
+            state.startUpdating(startcb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : LENode {
+            var curr : LENode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
         }
     }
 }
